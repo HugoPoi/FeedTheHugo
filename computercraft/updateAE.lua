194470b84c16c82c3b21d7eb38723a7c8f9ce3c6
HugoPoi <hugo.poissonnet@gmail.com>
2014-08-05 13:49:27 +0200

    add count

--- computercraft/updateAE.lua
+++ computercraft/updateAE.lua
@@ -1,12 +1,18 @@
-
 local meBridgeSide = 'back'
 local postDataUrl = 'http://fb2.poissonnet.fr/minecraft/'
 
+local JSON = (loadfile "jsonpurlua")()
 
 if peripheral.isPresent(meBridgeSide) and peripheral.getType(meBridgeSide) == "meBridge" then
   local meBridgeWrap = peripheral.wrap(meBridgeSide)
   local items = meBridgeWrap.listItems()
-  local postStatus = http.post(postDataUrl, encode(items))
+  local count = 0
+  local export = {}
+  for i,d in pairs(items) do
+      export[""..i] = d
+  end
+  local jsondata = JSON:encode(export)
+  local postStatus = http.post(postDataUrl, jsondata)
   if postStatus ~= nil then
     if postStatus.getResponseCode() == 200 then
       local response = postStatus.readAll()
@@ -16,5 +22,4 @@ if peripheral.isPresent(meBridgeSide) and peripheral.getType(meBridgeSide) == "m
     print("Connection to "..postDataUrl.." failed, server unaivalable.")
   end
 end
-
 
